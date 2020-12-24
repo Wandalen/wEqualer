182036f0820701063e93dbff3e7f643f872b25a7
Jack Shell <jackshel45@gmail.com>
2020-12-24 18:12:15 +0200

    Minor

--- proto/wtools/abase/l6/Equaler.s
+++ proto/wtools/abase/l6/Equaler.s
@@ -481,10 +481,11 @@ function entityDiff( src, src2, opts )
     path : opts.it.lastPath,
   });
 
-// = qqq for Yevhen : fix please
-//     test.identical( _.property.all( {} ), {} )
-//         - got :          {}
-//         - expected :     {}
+  /* = qqq for Yevhen : fix please
+       test.identical( _.property.all( {} ), {} )
+           - got :          {}
+           - expected :     {}
+  */
 
   return result;
 }
